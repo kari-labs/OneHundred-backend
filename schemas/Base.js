f38758aa1ff22b6a898ba04a04da1f547a8fdcb9
Ian fabs <ianfabs@outlook.com>
2019-08-16 22:45:02 -0400

    Added geo field to base modal

--- schemas/Base.js
+++ schemas/Base.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const baseSchema = new mongoose.Schema({
     data: String,
     owner: mongoose.Types.ObjectId,
+    geo: String,
 });
 
 const Base = mongoose.model('Base', baseSchema);
